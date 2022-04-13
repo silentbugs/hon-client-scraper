234c8937d01e4d276a58d653442d7898633e13c8
Dimitrios Misdanitis <d.misdanitis@gmail.com>
2022-04-13 05:15:57 +0300

    wip: working next() function on semvers
    requires a script to call it and provide the 'exists' variable

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import ntpath
 
 
+class SemverException(Exception):
+    pass
+
+
 class SemverUtil:
     def create(major, minor, patch, hotfix=None):
         version = f'{major}.{minor}.{patch}'
@@ -53,6 +57,42 @@ class Semver:
             self.major, self.minor, self.patch, hotfix
         )
 
+    def next(self, exists=True):
+        sv = Semver(self.__str__())
+
+        if exists:
+            if sv.hotfix is not None:
+                # for 4 digit semvers that exist, the next hotfix version
+                sv.hotfix += 1
+
+            if sv.hotfix is None:
+                # for 3 digit semvers that exist, the first hotfix version
+                sv.hotfix = 0
+
+        else:
+            if sv.hotfix is None:
+                if sv.minor == 0 and sv.patch == 0:
+                    # we ran out of versions
+                    raise SemverException('There is no next version')
+                elif sv.patch == 0:
+                    # for zero 3rd digit semvers, we probably ran out of minor
+                    # versions increment major
+                    sv.major += 1
+                    sv.minor = 0
+                    sv.patch = 0
+                else:
+                    # for non-zero 3rd digit semvers, increment 2nd digit,
+                    # set 3rd to 0
+                    sv.minor += 1
+                    sv.patch = 0
+
+            if sv.hotfix is not None:
+                # for 4 digit semvers that don't exist, remove 4th digit
+                # increment 3rd by 1
+                sv.hotfix = None
+                sv.patch += 1
+
+        return sv
 
 
 class FileUtil:
